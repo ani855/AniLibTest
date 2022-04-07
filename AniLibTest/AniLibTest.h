af25bb2563085eb5cf921ade538f74692961144c
Ani Klekchyan <ani.klekchyan@healios.io>
2022-04-07 12:44:17 +0400

    Initial Commit

---
+++ AniLibTest/AniLibTest.h
@@ -0,0 +1,18 @@
+//
+//  AniLibTest.h
+//  AniLibTest
+//
+//  Created by Ani Klekchyan Work on 07.04.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AniLibTest.
+FOUNDATION_EXPORT double AniLibTestVersionNumber;
+
+//! Project version string for AniLibTest.
+FOUNDATION_EXPORT const unsigned char AniLibTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AniLibTest/PublicHeader.h>
+
+
