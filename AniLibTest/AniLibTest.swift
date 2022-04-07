9763911dc7ddcaa4e028ed9bcf11ba521e2735d7
Ani Klekchyan <ani.klekchyan@healios.io>
2022-04-07 12:47:09 +0400

    first commit

---
+++ AniLibTest/AniLibTest.swift
@@ -0,0 +1,8 @@
+//
+//  AniLibTest.swift
+//  AniLibTest
+//
+//  Created by Ani Klekchyan Work on 07.04.22.
+//
+
+import Foundation
---
+++ AniLibTestApp/AniLibTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AniLibTestAppApp.swift
+//  AniLibTestApp
+//
+//  Created by Ani Klekchyan Work on 07.04.22.
+//
+
+import SwiftUI
+
+@main
+struct AniLibTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
